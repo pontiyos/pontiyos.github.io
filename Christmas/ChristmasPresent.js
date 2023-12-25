aea5555165ff620aeac178dc886ae8f765069ce8
pontiyos <erik.rosenstrom@gmail.com>
2023-12-25 13:24:34 +0100

    Close to final

--- Christmas/ChristmasPresent.js
+++ Christmas/ChristmasPresent.js
@@ -1,6 +1,6 @@
 
-var to = 'Alice ,Alma ,Ester , Saga, Malte, Lovisa';
-var gift_url = 'http://127.0.0.1:5500/Christmas/ChristmasPresent_text.png';
+var to = 'Alice ,Alma ,Ester , \r\n Saga, Malte, Lovisa';
+var gift_url = 'https://pontiyos.github.io/Christmas/ChristmasPresent_text.png';
 var gift_image_url = 'ChristmasPresent_text.png';
 
 
@@ -30,15 +30,75 @@ function init() {
       presentImage.appendChild(_giftImg);
     }
   }
-    
+  
+  /*
   present.addEventListener("click", function(e){
     present.classList.toggle("open");
   }, false);
-  
+  */
   
   
   nametag.innerText = to;
 }
-
 init();
 
+function checkAnswers(){
+    
+    const correctAnswers = ['sex','stygga','barn','döms','till','fängelse']; 
+
+    var answerAlice = $('#cracker-alice').val();
+    var answerAlma = $('#cracker-alma').val();
+    var answerEster = $('#cracker-ester').val();
+    var answerSaga = $('#cracker-saga').val();
+    var answerMalte = $('#cracker-malte').val();
+    var answerLovisa = $('#cracker-lovisa').val();
+
+    answerAlice = textTransform(answerAlice);
+    answerAlma = textTransform(answerAlma);
+    answerEster = textTransform(answerEster);
+    answerSaga = textTransform(answerSaga);
+    answerMalte = textTransform(answerMalte);
+    answerLovisa = textTransform(answerLovisa);
+
+    if (answerAlice == correctAnswers[0]){
+        if (answerMalte == correctAnswers[1]){
+            if (answerAlma == correctAnswers[2]){
+                if (answerEster == correctAnswers[3]){
+                    if (answerLovisa == correctAnswers[4]){
+                        if (answerSaga == correctAnswers[5]){
+                        return true;
+                    }
+                }            
+            }
+        }
+    }
+}
+
+    return false;
+}
+
+function openPresent (){
+    console.log("OPENING!");
+    const currentUrl = window.location.href;
+    window.location =  currentUrl+"#present"
+    if(checkAnswers()){
+        present.classList.toggle("open");
+    }
+    else{
+        alert("Tyvärr, bättre lycka nästa gång!");
+        window.location =  currentUrl+"#present-info-text"
+        location.reload();
+    }
+
+}
+
+
+function textTransform (string){
+    /* 
+        Remove all charactes which are not letters (A-Z)
+        Change the text to lowercase
+    */
+    string = string.replaceAll("[^a-zA-Z]", " ");
+    string = string.toLowerCase();
+    return string;
+}
---
+++ Christmas/OpenPresent.js
@@ -0,0 +1,11 @@
+/*
+
+Get the values 
+Check if they match
+If NOT - ERROR
+If they do 
+    focus the package
+    Open the package
+    
+
+*/ 
