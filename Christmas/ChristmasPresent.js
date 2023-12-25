141aa6932878b07096044c773799f2eb3eb8fc85
pontiyos <erik.rosenstrom@gmail.com>
2023-12-25 09:22:58 +0100

    ...

---
+++ Christmas/ChristmasPresent.js
@@ -0,0 +1,44 @@
+
+var to = 'Alice ,Alma ,Ester , Saga, Malte, Lovisa';
+var gift_url = 'http://127.0.0.1:5500/Christmas/ChristmasPresent_text.png';
+var gift_image_url = 'ChristmasPresent_text.png';
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
