9fe5e9577a506ae4fa43320dc61d885d969c8080
pontiyos <erik.rosenstrom@gmail.com>
2024-03-17 22:06:59 +0100

    Updated certs

--- js/main.js
+++ js/main.js
@@ -18,15 +18,7 @@ function myFunction() {
     }
 }
 
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-    var x = document.getElementById("navBarSmall");
-    if (x.className.indexOf("w3-show") == -1) {
-        x.className += " w3-show";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
+
 
 
 
